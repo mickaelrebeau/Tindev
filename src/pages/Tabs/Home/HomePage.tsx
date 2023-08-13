7472e7a3cc0a27987b0e8eb34aa48e067d4cc07f
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-08-13 07:09:53 +0200

    Notifications Page

--- src/pages/Tabs/Home/HomePage.tsx
+++ src/pages/Tabs/Home/HomePage.tsx
@@ -1,12 +1,8 @@
-import { IonButton, IonContent, IonHeader, IonIcon, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButton, IonContent, IonHeader, IonIcon, IonToolbar } from "@ionic/react";
 import flamme from '../../../assets/flame-outline.svg'
 import notif from '../../../assets/notifications-outline.svg'
-import settings from '../../../assets/settings-outline.svg'
 import heart from '../../../assets/heart.svg'
-import back from '../../../assets/refresh-outline.svg'
 import skip from '../../../assets/close.svg'
-import star from '../../../assets/star-outline.svg'
-import certif from '../../../assets/medical.svg'
 import alert from '../../../assets/alert-outline.svg'
 import './Home.css'
 
@@ -22,10 +18,9 @@ export default function HomePage() {
               <h2>tindev</h2>
             </div>
 
-            <div className="home-header-btn">
-              <IonIcon slot="start" size="large" icon={notif}></IonIcon>
-              <IonIcon slot="start" size="large" icon={settings}></IonIcon>
-            </div>
+            <IonButton fill="clear" size="large" routerLink="/tabs/home/notifications">
+              <IonIcon icon={notif}></IonIcon>
+            </IonButton>
           </div>
         </IonToolbar>
 
@@ -33,25 +28,16 @@ export default function HomePage() {
 
       <IonContent className="ion-padding">
         <div className="home-card">
-          <h2 className="home-title">
-            Admin 99
-            <IonIcon slot="end" icon={certif}></IonIcon>
-          </h2>
+          <h2>Admin 99</h2>
           <div className="home-actions">
-            <IonButton fill="outline" size="small" className="home-small-btn">
-              <IonIcon icon={back}></IonIcon>
-            </IonButton>
             <IonButton fill="outline" size="large" className="home-lareg-btn">
               <IonIcon icon={skip}></IonIcon>
             </IonButton>
             <IonButton fill="outline" size="small" className="home-small-btn">
-              <IonIcon icon={star}></IonIcon>
+              <IonIcon icon={alert}></IonIcon>
             </IonButton>
             <IonButton fill="outline" size="large" className="home-lareg-btn">
               <IonIcon icon={heart}></IonIcon>
-            </IonButton>
-            <IonButton fill="outline" size="small" className="home-small-btn">
-              <IonIcon icon={alert}></IonIcon>
             </IonButton>
           </div>
         </div>
---
+++ src/pages/Tabs/Home/NotificationsPage.tsx
@@ -0,0 +1,34 @@
+import { IonBackButton, IonBadge, IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonTitle, IonToolbar } from "@ionic/react";
+import './Home.css'
+
+export default function NotificationsPage() {
+  return (
+    <>
+      <IonHeader>
+
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/tabs/home"></IonBackButton>
+          </IonButtons>
+          <IonTitle> Notifications </IonTitle>
+        </IonToolbar>
+
+      </IonHeader>
+
+      <IonContent className="ion-padding">
+        <IonItem>
+          <IonBadge slot="end" color="success">1</IonBadge>
+          <IonLabel>Voici un exemple de notifications</IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonBadge slot="end" color="success">+99</IonBadge>
+          <IonLabel>Vous avez de nouveax likes</IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonBadge slot="end" color="success">5</IonBadge>
+          <IonLabel>Vous avez de nouveax matchs</IonLabel>
+        </IonItem>
+      </IonContent>
+    </>
+  );
+}
