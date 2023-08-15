7b543e2d2fc6ae38e01b044e1393a8dbb7035ff1
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-08-16 01:29:53 +0200

    Settings Content

--- src/pages/Settings/SettingsPage.tsx
+++ src/pages/Settings/SettingsPage.tsx
@@ -1,4 +1,5 @@
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/react";
+import { IonActionSheet, IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/react";
+import './Settings.css';
 
 export default function SettingsPage() {
   return (
@@ -9,14 +10,68 @@ export default function SettingsPage() {
           <IonButtons slot="start">
             <IonBackButton defaultHref="/tabs/profil"></IonBackButton>
           </IonButtons>
-          <IonTitle> SettingsPage </IonTitle>
+          <IonTitle> Paramètres </IonTitle>
         </IonToolbar>
 
       </IonHeader>
 
       <IonContent className="ion-padding">
-        <h2>Settings</h2>
+        <div className="settings-content">
+          <div>
+            <h2>Notifications</h2>
+            <div className="settings-card">
+              <a>Adresse e-mail</a>
+              <a>Notification Push</a>
+            </div>
+          </div>
+          <div>
+            <h2>Contacte-nous</h2>
+            <div className="settings-card">
+              <a>Aide et assitance</a>
+            </div>
+          </div>
+          <div>
+            <h2>Confidentialités</h2>
+            <div className="settings-card">
+              <a>Politique relative aux cookies</a>
+              <a>Politique de confidentialité</a>
+              <a>Préférences de confidentialité</a>
+            </div>
+          </div>
+          <div>
+            <h2>Mentions légales</h2>
+            <div className="settings-card">
+              <a>Licenses</a>
+              <a>Conditions d'utilisation</a>
+            </div>
+          </div>
+          <div className="settings-btn">
+            <IonButton expand="full" routerLink="/">Déconnexion</IonButton>
+            <IonButton id="open-action-sheet" expand="full">Supprimer votre Compte</IonButton>
+          </div>
+        </div>
       </IonContent>
+
+      <IonActionSheet
+        trigger="open-action-sheet"
+        header="Etes-vous sûr de vouloir supprimer votre compte ?"
+        buttons={[
+          {
+            text: 'Supprimer',
+            role: 'destructive',
+            data: {
+              action: 'delete',
+            },
+          },
+          {
+            text: 'Annuler',
+            role: 'cancel',
+            data: {
+              action: 'cancel',
+            },
+          },
+        ]}
+      ></IonActionSheet>
     </>
   );
 }
