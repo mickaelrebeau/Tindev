8b7d54e2b11a8dfcbca71b661452488daedbee2f
Forthtilliath <forth.developer@gmail.com>
2023-08-31 18:40:36 +0200

    Center header when no children

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,8 @@
 import { IonHeader, IonIcon, IonToolbar } from "@ionic/react";
 import { PropsWithChildren } from "react";
 import IconFlamme from "../../assets/flame-outline.svg";
+import styles from "./Header.module.css";
+import clsx from "clsx";
 
 type Props = PropsWithChildren<{}>;
 
@@ -8,13 +10,13 @@ export function Header({ children }: Props) {
   return (
     <IonHeader>
       <IonToolbar>
-        <div className="msg-header">
-          <div className="msg-logo">
+        <div className={clsx(styles.wrapper, { [styles.center]: !children })}>
+          <div className={styles.titleWrapper}>
             <IonIcon slot="start" size="large" icon={IconFlamme} />
-            <h2>tindev</h2>
+            <h2 className={styles.title}>tindev</h2>
           </div>
 
-          <div className="profil-header-btn">{children}</div>
+          <div className={styles.buttonsWrapper}>{children}</div>
         </div>
       </IonToolbar>
     </IonHeader>
