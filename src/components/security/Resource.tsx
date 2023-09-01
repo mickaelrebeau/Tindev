3ce101ee5197ccf23f3022198d386fb08f55fa94
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-09-01 13:49:07 +0200

    Module Css Updates

--- src/components/security/Resource.tsx
+++ src/components/security/Resource.tsx
@@ -11,14 +11,14 @@ type Props = {
 export function Resource({ title, phoneNumber, link }: Props) {
   return (
     <>
-      <h2>{title}</h2>
+      <h2 className={styles.contentTitle}>{title}</h2>
       {phoneNumber && (
         <div className={styles.call}>
           <IonIcon size="small" icon={IconCall} />
           <p>{phoneNumber}</p>
         </div>
       )}
-      <IonButton size="small" fill="outline" href={link}>
+      <IonButton size="small" shape="round" fill="outline" href={link}>
         Aller sur le site
       </IonButton>
     </>
